0b49701a6dc8988c9634ee15453885fcfc692bcc
Sam Pierce <sdinespierce@gmail.com>
2021-08-10 15:50:51 -0400

    can now insert data into table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,24 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+tbody = d3.select("tbody");
+
+// loop through data, create row, insert into td cells
+// could do only 50chars of comments with a see more option that expands it
+
+//TODO: right justify, Capitalize City Names, fix &#44 string in comments field
+tableData.forEach((report) => {
+    let row = tbody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+        let cell = row.append("td");
+        cell.text(value);
+    }); 
+})
+
+
+// filter from html form
+
+
+
+// bonus: filters for each of the columns --- on change
+
